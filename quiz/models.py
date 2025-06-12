53f372a200cd6c1a76523f163bef71de54a33737
JUSTINHARRY <justinharryjayd@gmail.com>
2025-06-12 04:08:30 +0100

    feat: add models for basic quiz functionality
    Basic functionality includes access to quiz questions and results

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+
+
+class Question(models.Model):
+    content = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+
+class Option(models.Model):
+    content = models.CharField(max_length=255)
+    is_correct = models.BooleanField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class Result(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    duration = models.DurationField()
+
+
+class AnsweredQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    result = models.ForeignKey(
+        Result,
+        on_delete=models.CASCADE,
+        related_name="answered_questions",
+    )
