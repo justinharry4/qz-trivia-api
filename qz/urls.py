9bae33374a62e512a74b30546844d28e01a9f81c
JUSTINHARRY <justinharryjayd@gmail.com>
2025-07-26 17:14:05 +0100

    fix: fix indentation in root urls module

--- qz/urls.py
+++ qz/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
 
 if settings.DEBUG:
 	from debug_toolbar.toolbar import debug_toolbar_urls
-	
+	urlpatterns += debug_toolbar_urls()
+
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += debug_toolbar_urls()
