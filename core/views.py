579fd1fc916a4e30cbe580a09abdd266a2a17f28
JUSTINHARRY <justinharryjayd@gmail.com>
2025-07-26 19:29:30 +0100

    feature: allow all safe http methods at /health/
    This ensures Uptime Robot receives a 200 OK response when monitoring
    the API using HEAD requests

--- core/views.py
+++ core/views.py
@@ -2,6 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'HEAD', 'OPTIONS'])
 def health_check(request):
 	return Response({'status': 'OK'}, status.HTTP_200_OK)
