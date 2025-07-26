606bf8e1ac0e7e510febb2c7a38ef4d7dc636daf
JUSTINHARRY <justinharryjayd@gmail.com>
2025-07-26 17:07:03 +0100

    fix: move debug toolbar import to dev-only block

--- qz/urls.py
+++ qz/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 # from django.conf.urls.static import static
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
@@ -12,5 +11,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+	from debug_toolbar.toolbar import debug_toolbar_urls
+	
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += debug_toolbar_urls()
