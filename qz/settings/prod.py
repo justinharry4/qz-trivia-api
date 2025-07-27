1bd80fbe0e9a1d44ae1af2749ee974292e4c2e40
JUSTINHARRY <justinharryjayd@gmail.com>
2025-07-27 15:10:31 +0100

    fix: read client url as an environment variable

--- qz/settings/prod.py
+++ qz/settings/prod.py
@@ -7,9 +7,9 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ["qz-trivia-api.onrender.com"]
+ALLOWED_HOSTS = [os.environ.get('SITE_HOST_NAME')]
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [os.environ.get('CLIENT_URL')]
 
 DATABASES = {
     "default": dj_database_url.config()
